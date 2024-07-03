dee95aa1b4af3621afd98c6db688e15ebd323b2c
AndriiIladii <iladiy21@gmail.com>
2024-07-03 22:21:59 +0300

    added rtk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import * as styles from "./App.module.css";
 
 const App = () => {
-  return <div className={styles.item}>Hello Andrii</div>;
+  return <div>Hello Andrii</div>;
 };
 
 export default App;
+ff;

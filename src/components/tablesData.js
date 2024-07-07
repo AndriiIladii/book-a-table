3dc641d8c0dc575288b5dcca3fdd3f48679b3ba5
AndriiIladii <iladiy21@gmail.com>
2024-07-07 21:55:34 +0300

    added svg components render

---
+++ src/components/tablesData.js
@@ -0,0 +1,413 @@
+const tables = [
+  {
+    table: {
+      number: 1,
+      x: 462.61,
+      y: 702.1,
+      width: 51.57,
+      height: 27.91,
+    },
+    text: {
+      transform: "translate(485.46 719.97)",
+    },
+  },
+  {
+    table: {
+      number: 2,
+      x: 462.48,
+      y: 617.71,
+      width: 51.19,
+      height: 26.81,
+    },
+    text: {
+      transform: "translate(484.87 636.22)",
+    },
+  },
+  {
+    table: {
+      number: 3,
+      x: 461.59,
+      y: 534.22,
+      width: 52.09,
+      height: 26.04,
+    },
+    text: {
+      transform: "translate(484.48 552.54)",
+    },
+  },
+  {
+    table: {
+      number: 4,
+      x: 461.59,
+      y: 452.73,
+      width: 50.72,
+      height: 26.72,
+    },
+    text: {
+      transform: "translate(483.58 471.27)",
+    },
+  },
+  {
+    table: {
+      number: 5,
+      x: 459.71,
+      y: 372.05,
+      width: 51.57,
+      height: 25.7,
+    },
+    text: {
+      transform: "translate(481.67 389.23)",
+    },
+  },
+  {
+    table: {
+      number: 6,
+      x: 459.71,
+      y: 291.88,
+      width: 50.72,
+      height: 26.38,
+    },
+    text: {
+      transform: "translate(481.84 308.97)",
+    },
+  },
+  {
+    table: {
+      number: 7,
+      x: 460.69,
+      y: 211.76,
+      width: 50.55,
+      height: 25.53,
+    },
+    text: {
+      transform: "translate(482.9 229.24)",
+    },
+  },
+  {
+    table: {
+      number: 8,
+      x: 461.46,
+      y: 132.35,
+      width: 50.55,
+      height: 25.15,
+    },
+    text: {
+      transform: "translate(483.22 150.74)",
+    },
+  },
+  {
+    table: {
+      number: 9,
+      x: 392.39,
+      y: 177.67,
+      width: 50.68,
+      height: 25.02,
+    },
+    text: {
+      transform: "translate(414.35 194.65)",
+    },
+  },
+  {
+    table: {
+      number: 10,
+      x: 391.29,
+      y: 255.12,
+      width: 51.06,
+      height: 25.02,
+    },
+    text: {
+      transform: "translate(410.52 271.63)",
+    },
+  },
+  {
+    table: {
+      number: 11,
+      x: 391.29,
+      y: 385.43,
+      width: 51.06,
+      height: 26.62,
+    },
+    text: {
+      transform: "translate(410.52 404.03)",
+    },
+  },
+  {
+    table: {
+      number: 12,
+      x: 392.39,
+      y: 466.01,
+      width: 50.81,
+      height: 25.53,
+    },
+    text: {
+      transform: "translate(410.63 483.78)",
+    },
+  },
+  {
+    table: {
+      number: 13,
+      x: 392.39,
+      y: 592.31,
+      width: 51.57,
+      height: 26.68,
+    },
+    text: {
+      transform: "translate(411.97 610.08)",
+    },
+  },
+  {
+    table: {
+      number: 14,
+      x: 313.76,
+      y: 583.88,
+      width: 51.32,
+      height: 27.45,
+    },
+    text: {
+      transform: "translate(332.62 601.99)",
+    },
+  },
+  {
+    table: {
+      number: 15,
+      x: 313.76,
+      y: 478.61,
+      width: 50.64,
+      height: 25.87,
+    },
+    text: {
+      transform: "translate(332.71 496.37)",
+    },
+  },
+  {
+    table: {
+      number: 16,
+      x: 313.76,
+      y: 374.1,
+      width: 50.64,
+      height: 26.89,
+    },
+    text: {
+      transform: "translate(333.41 392.61)",
+    },
+  },
+  {
+    table: {
+      number: 17,
+      x: 313.76,
+      y: 271.63,
+      width: 49.11,
+      height: 26.04,
+    },
+    text: {
+      transform: "translate(332.28 289.14)",
+    },
+  },
+  {
+    table: {
+      number: 18,
+      x: 314.27,
+      y: 169.88,
+      width: 50.04,
+      height: 24.77,
+    },
+    text: {
+      transform: "translate(332.59 186.73)",
+    },
+  },
+  {
+    table: {
+      number: 19,
+      cx: 471.96,
+      cy: 51.78,
+      rx: 21.19,
+      ry: 21.7,
+    },
+    text: {
+      transform: "translate(464.98 57.69)",
+    },
+  },
+  {
+    table: {
+      number: 20,
+      cx: 383.64,
+      cy: 54.09,
+      rx: 21.19,
+      ry: 21.7,
+    },
+    text: {
+      transform: "translate(377.06 59.14)",
+    },
+  },
+  {
+    table: {
+      number: 21,
+      cx: 294.41,
+      cy: 54.09,
+      rx: 21.19,
+      ry: 21.7,
+    },
+    text: {
+      transform: "translate(287.83 59.14)",
+    },
+  },
+  {
+    table: {
+      number: 22,
+      cx: 236.55,
+      cy: 211.46,
+      rx: 21.19,
+      ry: 21.7,
+    },
+    text: {
+      transform: "translate(229.63 215.2)",
+    },
+  },
+  {
+    table: {
+      number: 23,
+      cx: 148.48,
+      cy: 221.46,
+      rx: 21.19,
+      ry: 21.7,
+    },
+    text: {
+      transform: "translate(141.78 225.63)",
+    },
+  },
+  {
+    table: {
+      number: 24,
+      x: 38.61,
+      y: 198.78,
+      width: 24.34,
+      height: 52.6,
+    },
+    text: {
+      transform: "translate(43.7 226.05)",
+    },
+  },
+  {
+    table: {
+      number: 25,
+      x: 37.07,
+      y: 302.78,
+      width: 24.43,
+      height: 51.66,
+    },
+    text: {
+      transform: "translate(42.93 329.65)",
+    },
+  },
+  {
+    table: {
+      number: 26,
+      x: 70.61,
+      y: 458.52,
+      width: 29.87,
+      height: 29.87,
+    },
+    text: {
+      transform: "translate(78.6 477.67)",
+    },
+  },
+  {
+    table: {
+      number: 27,
+      x: 67.88,
+      y: 559.29,
+      width: 30.47,
+      height: 30.47,
+    },
+    text: {
+      transform: "translate(76.95 579.31)",
+    },
+  },
+  {
+    table: {
+      number: 28,
+      x: 62.1,
+      y: 707.54,
+      width: 51.74,
+      height: 27.06,
+    },
+    text: {
+      transform: "translate(81.71 725.23)",
+    },
+  },
+  {
+    table: {
+      number: 29,
+      x: 140.56,
+      y: 705.84,
+      width: 51.91,
+      height: 27.57,
+    },
+    text: {
+      transform: "translate(159.67 724.06)",
+    },
+  },
+  {
+    table: {
+      number: 30,
+      x: 1.5,
+      y: 605.84,
+      width: 42.89,
+      height: 170.55,
+    },
+    text: {
+      transform: "translate(28.42 702.84) rotate(-90)",
+    },
+  },
+  {
+    table: {
+      number: 40,
+      x: 254.27,
+      y: 504.48,
+      width: 30.64,
+      height: 30.64,
+    },
+    text: {
+      transform: "translate(263.12 524.61)",
+    },
+  },
+  {
+    table: {
+      number: 41,
+      x: 254.9,
+      y: 430.82,
+      width: 30.32,
+      height: 30.32,
+    },
+    text: {
+      transform: "translate(263.97 450.3)",
+    },
+  },
+  {
+    table: {
+      number: 42,
+      x: 255.03,
+      y: 357.54,
+      width: 30,
+      height: 30,
+    },
+    text: {
+      transform: "translate(263.34 376.89)",
+    },
+  },
+
+  {
+    table: {
+      number: 45,
+      x: 58.18,
+      y: 44.9,
+      width: 115.66,
+      height: 25.79,
+    },
+    text: {
+      transform: "translate(108.19 64.15)",
+    },
+  },
+];
+
+export default tables;

8c666f4d4b4e9211a78e217625f72cb86bea28be
AndriiIladii <iladiy21@gmail.com>
2024-07-08 17:13:19 +0300

    added saving booking data from modal and cache saving in rtk

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addTable, saveTable, loadTable } from "../store/TableSlice";
 import * as styles from "./Modal.module.css";
 
 const Modal = ({ active, setActive, tableNumber }) => {
+  const [tableName, setTableName] = useState("");
+  const [guestCount, setGuestCount] = useState(0);
+  const [reservationDate, setReservationDate] = useState(new Date());
+  const [reservationTime, setReservationTime] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(null);
+  const [birthday, setBirthday] = useState(false);
+  const [notes, setNotes] = useState("");
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTable());
+
+    const handleBeforeUnload = () => {
+      dispatch(saveTable());
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  function addNewTable() {
+    const newReservation = {
+      name: tableName,
+      tableNumber: tableNumber,
+      guests: guestCount,
+      date: reservationDate,
+      time: reservationTime,
+      tel: phoneNumber,
+      holiday: birthday,
+      comment: notes,
+    };
+
+    dispatch(addTable(newReservation));
+    setActive(false);
+  }
+
   return (
     <>
       {active && (
@@ -16,38 +57,62 @@ const Modal = ({ active, setActive, tableNumber }) => {
               <div className={styles.first}>
                 <div>
                   <label>Name</label>
-                  <input type="text" />
+                  <input
+                    type="text"
+                    onChange={(e) => setTableName(e.target.value)}
+                  />
                 </div>
                 <div>
                   <label>Guest count</label>
-                  <input type="number" />
+                  <input
+                    type="number"
+                    onChange={(e) => setGuestCount(e.target.value)}
+                  />
                 </div>
                 <div>
                   <label>Booking date</label>
-                  <input type="date" />
+                  <input
+                    type="date"
+                    onChange={(e) => setReservationDate(e.target.value)}
+                  />
                 </div>
               </div>
               <div className={styles.second}>
                 <div>
                   <label>Booking Time</label>
-                  <input type="time" />
+                  <input
+                    type="time"
+                    onChange={(e) => setReservationTime(e.target.value)}
+                  />
                 </div>
                 <div>
                   <label>Phone number</label>
-                  <input type="tel" />
+                  <input
+                    type="tel"
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
                 </div>
                 <div>
                   <label>Has birthday</label>
-                  <input type="checkbox" />
+                  <input
+                    type="checkbox"
+                    onChange={(e) => setBirthday(e.target.checked)}
+                  />
                 </div>
               </div>
             </form>
             <div className={styles.commentary}>
               <label>Notes</label>
-              <input type="text" />
+              <input type="text" onChange={(e) => setNotes(e.target.value)} />
             </div>
             <div>
-              <button type="submit">Add Reservation</button>
+              <button
+                onClick={addNewTable}
+                className={styles.formBtn}
+                type="submit"
+              >
+                Add Reservation
+              </button>
             </div>
           </div>
         </div>
--- src/store/TableSlice.jsx
+++ src/store/TableSlice.jsx
@@ -8,8 +8,19 @@ export const TableSlice = createSlice({
   name: "table",
   initialState,
   reducers: {
-    addTable: (state) => {},
+    addTable: (state, action) => {
+      state.table.push(action.payload);
+    },
+    saveTable: (state) => {
+      localStorage.setItem("newTable", JSON.stringify(state.table));
+    },
+    loadTable: (state) => {
+      const storedTable = JSON.parse(localStorage.getItem("newTable")) || [];
+      state.table = storedTable;
+    },
   },
 });
+
+export const { addTable, saveTable, loadTable } = TableSlice.actions;
 
 export default TableSlice.reducer;

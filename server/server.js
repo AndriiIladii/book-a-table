0c5bcf83292b2a2e2f26f97bd10cca06fd532534
AndriiIladii <iladiy21@gmail.com>
2024-08-07 01:43:45 +0300

    added create and read functions on server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
+
+const folderPath = "./server";
+
+const filePath = path.join(folderPath, "reservations.json");
 
 const app = express();
 const PORT = 5000;
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res) => {
+app.get("/api/reservations", (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
   res.json({ message: "Hello from Express!" });
+});
+
+app.post("/api/reservations", (req, res) => {
+  try {
+    fs.writeFileSync(filePath, content);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(PORT, () => {
